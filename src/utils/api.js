6b48d3ccf181f3dbc37816152c26c17510884533
ZaNeih <669830@stud.hvl.no>
2026-06-10 15:40:28 +0200

    lagde en gitognore, og env fil. deretter en API key på env fil slik at den ikke vises på github

--- src/utils/api.js
+++ src/utils/api.js
@@ -26,7 +26,7 @@ const response = await fetch(CLAUDE_API_URL, {
         "anthropic-dangerous-direct-browser-access": "true",
     },
     body: JSON.stringify({
-        model:  "claude-sonnet-4-20250514",
+        model: "claude-sonnet-4-5",
         max_tokens: 1000,
         messages:[{role:"user", content: prompt}]
     }),
